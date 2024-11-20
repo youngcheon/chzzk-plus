399bc12ff24ff3346b58850da1ddd3b73669957e
youngcheon <kimyoungcheon97@gmail.com>
2024-11-20 15:12:21 +0900

    feat : #54/자동 넓은 화면

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -9,14 +9,8 @@ export const createReactElement = (
 };
 
 export async function waitingElement(selector: string): Promise<HTMLElement> {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  return await new Promise((resolve, _reject) => {
-    const interval = setInterval(() => {
-      const element = document.querySelector(selector);
-      if (element !== null) {
-        clearInterval(interval);
-        resolve(element as HTMLElement);
-      }
-    }, 500);
-  });
+  while (document.querySelector(selector) === null) {
+    await new Promise((resolve) => requestAnimationFrame(resolve));
+  }
+  return document.querySelector(selector) as HTMLElement;
 }
